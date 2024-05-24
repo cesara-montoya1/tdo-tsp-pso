2d120c9a4692a874c3ddfc15f671f09a62251cda
donCESAR12345 <sprit152009@gmail.com>
2024-05-24 11:26:48 -0500

    [Refactor] Removed main
    Signed-off-by: donCESAR12345 <sprit152009@gmail.com>

--- pso.py
+++ pso.py
@@ -1,7 +1,6 @@
 import random
-import math
 import matplotlib.pyplot as plt
-from util import City, read_cities, write_cities_and_return_them, generate_cities, path_cost
+from util import read_cities, path_cost
 
 
 class Particle:
@@ -44,7 +43,8 @@ class PSO:
         return random.sample(self.cities, len(self.cities))
 
     def initial_population(self):
-        random_population = [self.random_route() for _ in range(self.population_size - 1)]
+        random_population = [self.random_route()
+                             for _ in range(self.population_size - 1)]
         greedy_population = [self.greedy_route(0)]
         return [*random_population, *greedy_population]
         # return [*random_population]
@@ -54,7 +54,8 @@ class PSO:
         del unvisited[start_index]
         route = [self.cities[start_index]]
         while len(unvisited):
-            index, nearest_city = min(enumerate(unvisited), key=lambda item: item[1].distance(route[-1]))
+            index, nearest_city = min(
+                enumerate(unvisited), key=lambda item: item[1].distance(route[-1]))
             route.append(nearest_city)
             del unvisited[index]
         return route
@@ -90,16 +91,19 @@ class PSO:
 
                 for i in range(len(self.cities)):
                     if new_route[i] != particle.pbest[i]:
-                        swap = (i, particle.pbest.index(new_route[i]), self.pbest_probability)
+                        swap = (i, particle.pbest.index(
+                            new_route[i]), self.pbest_probability)
                         temp_velocity.append(swap)
                         new_route[swap[0]], new_route[swap[1]] = \
                             new_route[swap[1]], new_route[swap[0]]
 
                 for i in range(len(self.cities)):
                     if new_route[i] != gbest[i]:
-                        swap = (i, gbest.index(new_route[i]), self.gbest_probability)
+                        swap = (i, gbest.index(
+                            new_route[i]), self.gbest_probability)
                         temp_velocity.append(swap)
-                        gbest[swap[0]], gbest[swap[1]] = gbest[swap[1]], gbest[swap[0]]
+                        gbest[swap[0]], gbest[swap[1]
+                                              ] = gbest[swap[1]], gbest[swap[0]]
 
                 particle.velocity = temp_velocity
 
@@ -110,24 +114,3 @@ class PSO:
 
                 particle.route = new_route
                 particle.update_costs_and_pbest()
-
-
-if __name__ == "__main__":
-    cities = read_cities(64)
-    pso = PSO(iterations=1200, population_size=300, pbest_probability=0.9, gbest_probability=0.02, cities=cities)
-    pso.run()
-    print(f'cost: {pso.gbest.pbest_cost}\t| gbest: {pso.gbest.pbest}')
-
-    x_list, y_list = [], []
-    for city in pso.gbest.pbest:
-        x_list.append(city.x)
-        y_list.append(city.y)
-    x_list.append(pso.gbest.pbest[0].x)
-    y_list.append(pso.gbest.pbest[0].y)
-    fig = plt.figure(1)
-    fig.suptitle('pso TSP')
-
-    plt.plot(x_list, y_list, 'ro')
-    plt.plot(x_list, y_list)
-    plt.show(block=True)
-
